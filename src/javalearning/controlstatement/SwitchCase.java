e23f131666a6a1c31badb036a22bac779d3a1c80
Rahenudden <33033966+Rahenudden@users.noreply.github.com>
2017-10-23 12:51:16 -0400

    Switch

--- src/javalearning/controlstatement/SwitchCase.java
+++ src/javalearning/controlstatement/SwitchCase.java
@@ -12,6 +12,7 @@ package javalearning.controlstatement;
 public class SwitchCase {
     public static void main(String [] args){
         System.out.println("Hello");
+        System.out.println("Hello git");
     }
     
 }
