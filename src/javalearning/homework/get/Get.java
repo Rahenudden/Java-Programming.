7166223740e19c1fd80b24e61bbe033c4abad871
Rahenudden <33033966+Rahenudden@users.noreply.github.com>
2018-01-27 17:13:29 -0500

    All

---
+++ src/javalearning/homework/get/Get.java
@@ -0,0 +1,50 @@
+
+package javalearning.homework.get;
+
+
+public class Get {
+
+    private int Emp_id;
+    private String Emp_name;
+    private double phone_number;
+
+    public Get() 
+    {
+    }
+
+    public Get(String Emp_name, int Emp_id, double phone_number ) 
+    {
+        this.Emp_name = Emp_name;
+        this.Emp_id = Emp_id;
+        this.phone_number = phone_number;
+  
+    }
+    public void set1 (int Emp_id)
+    {
+        this.Emp_id = Emp_id;
+    }
+    public void set2 (String Emp_name)
+    {
+        this.Emp_name = Emp_name;
+    }
+    
+    public  int get1  (int Emp_id) // formula of Get = public + Data type of variable + mehod name. (No parameter)
+    {
+        return Emp_id ;
+    }
+     public String get2 (String Emp_name)
+    {
+        return Emp_name ;
+    }
+
+    public void print () 
+    {
+    System.out.println(Emp_id) ;
+    System.out.println(Emp_name) ;
+    
+    }
+    
+   
+            
+    
+}
---
+++ src/javalearning/homework/get/PrintGet.java
@@ -0,0 +1,23 @@
+
+package javalearning.homework.get;
+
+
+public class PrintGet {
+    
+    public static void main (String []agrs)
+    
+   
+    
+    {
+        Get get = new Get();
+        System.out.println(get.get1(10)); // This way have to print  return variable.
+        System.out.println(get.get2("Rahen"));
+        
+        get.print();
+        
+    
+    }
+    
+    
+    
+}
